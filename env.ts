ca3e39f1413a02c187c1d73920252084e8630ce9
Natanael Lima <taelima1997@gmail.com>
2020-11-26 19:53:50 -0300

    integrated sentry

--- env.ts
+++ env.ts
@@ -1,4 +1,5 @@
 export default {
   API_URL: __DEV__ ? 'http://10.0.3.2:3335' : 'https://api.pi.mundotech.dev',
-  SENTRY_DNS: null,
+  SENTRY_DNS:
+    'https://7e6ef5441f4449f0b6672470ad415f69@o351938.ingest.sentry.io/5534222',
 };
